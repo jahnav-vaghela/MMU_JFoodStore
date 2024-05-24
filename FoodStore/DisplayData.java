5b92188b54a236188cd0aa8e057dc07f2239921d
Jahnav <jahnav.vaghela@gmail.com>
2024-05-24 12:56:11 +0100

    github upload

---
+++ FoodStore/DisplayData.java
@@ -0,0 +1,16 @@
+/**
+ * public interface for displaying data
+ */
+public interface DisplayData {
+
+  /**
+   * display all perametors
+   */
+  public void showAll();
+
+  /**
+   * display all products
+   */
+  public String toString();
+
+}
